b2db0c1a1d31956f0be23e97b898b1a8492c0b71
DopeCodes254 <njengajames724@gmail.com>
2022-02-11 12:40:08 +0300

    user login system with email verification and password reset

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'user-verification');
+
+define('EMAIL', '[ENTER YOUR EMAIL ACCOUNT]');
+define('PASSWORD', '[ENTER PASSWORD TO THE EMAIL ACCOUNT');
