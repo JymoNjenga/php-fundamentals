cd28f025abecfdd982094c3f5a35b9d33eaa834e
DopeCodes254 <njengajames724@gmail.com>
2022-04-04 21:55:11 +0300

    various outputs in php

---
+++ output.php
@@ -0,0 +1,34 @@
+<?php
+
+// echo - Outputs strings, numbers, floats, html etc.
+// echo 'Hello World', 123, 11.6;
+
+// print - Works like echo but only takes a single argument.
+// print 'Hello php programmer';
+
+// print_r() - Prints single values and arrays.
+// print_r(['james', 'david', 'francis']);
+
+// var_dump() - Returns more info like data type and length.
+// var_dump(false);
+// var_dump(['frodo', 'samwise', 'pipin', 'merry']);
+
+// var_export() - Similar to var_dump(). Outputs a string representation of a variable.
+// var_export('Hello');
+// var_export('This guy looks familiar.');
+// var_export(1234);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h2><?php echo 'php is a cool language.'; ?></h2>  
+</body>
+</html>
